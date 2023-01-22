c8823ddf25c3474e7c3bd55e0b96e553c5f918b5
ann <dorisann.me@gmail.com>
2023-01-22 21:01:37 +0000

    file upload

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const { fileUpload } = require("../controllers/posts");
+const multer = require("multer");
+const upload = multer({ dest: "uploads/" });
 
 const router = express.Router();
 
-router.post("/create", fileUpload);
+router.post("/create", upload.single("file"), fileUpload);
 
 module.exports = router;
